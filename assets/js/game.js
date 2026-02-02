4ec55c5e6802bf91dd615fff5ad998b4bbad4a28
Vincent Cauquil <131886402+EnderCryme@users.noreply.github.com>
2026-02-02 13:25:23 +0100

    Update game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,25 @@
-const startBtn = document.getElementById("start-btn");
-const startScreen = document.getElementById("start-screen");
-const gameWorld = document.getElementById("game-world");
+const player = document.getElementById("player");
 
-startBtn.addEventListener("click", () => {
-  startScreen.classList.remove("active");
-  gameWorld.classList.add("active");
+let x = 50;
+let y = window.innerHeight * 0.6;
+const speed = 5;
+
+document.addEventListener("keydown", (e) => {
+  switch (e.key.toLowerCase()) {
+    case "z":
+      y -= speed;
+      break;
+    case "s":
+      y += speed;
+      break;
+    case "q":
+      x -= speed;
+      break;
+    case "d":
+      x += speed;
+      break;
+  }
+
+  player.style.left = x + "px";
+  player.style.top = y + "px";
 });
