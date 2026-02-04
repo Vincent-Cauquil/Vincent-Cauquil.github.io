bb4c94b096221f9339c291d66cce2c1464d42fd6
Vincent Cauquil <131886402+EnderCryme@users.noreply.github.com>
2026-02-04 10:09:53 +0100

    Update macro.js

--- assets/js/macro.js
+++ assets/js/macro.js
@@ -1,13 +1,12 @@
-const LINKS = {
+const MACROS = {
     CPE_Lyon: '<a href="https://www.cpe.fr/detail/psm-presentation-de-la-formation/" target="_blank">CPE Lyon</a>',
     CEA_Leti: '<a href="https://www.leti-cea.fr/cea-tech/leti" target="_blank">CEA-Leti</a>'
 };
 
 function parseMacros(text) {
-    return text.replace(/\\(\w+)/g, (match, key) => LINKS[key] || match);
+    return text.replace(/\\(\w+)/g, (match, key) => MACROS[key] || match);
 }
 
-// Applique les macros à tous les éléments avec la classe "macro"
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.macro').forEach(el => {
         el.innerHTML = parseMacros(el.innerHTML);
